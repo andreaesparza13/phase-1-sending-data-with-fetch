f33ab65af9df4d8b2a20647d6f7329e36070e31f
Andrea <acesparza05@gmail.com>
2022-07-05 11:51:25 -0500

    commit tree 7c4f01e2a0057b072d31f0dafcd82096999262d5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+function submitData(name, email) {
+    const test1 = fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {"Content-Type": "application/json", 
+        "Accept": "application/json",},
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+        .then(res => res.json())
+        .then(object => document.body.innerHTML = object["id"])
+        .catch(error => document.body.innerHTML = error.message)
+        return test1;
+    }
+
+// function submitData( name, email ) {
+//     return fetch( 'http://localhost:3000/users', {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify( {
+//           name,
+//           email
+//         } )
+//       } )
+//       .then( function ( response ) {
+//         return response.json()
+//       } )
+//       .then( function ( object ) {
+//         document.body.innerHTML = object[ "id" ]
+//       } )
+//       .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+//   }
